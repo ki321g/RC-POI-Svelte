3d464c0ceba5f366aabbd351c48c451641aae2c8
Kieron Garvey <garveykieron@gmail.com>
2024-05-17 16:16:49 +0100

    changing env file

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -1,16 +1,16 @@
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private'
+import { VITE_FB_CLIENT_EMAIL, VITE_FB_PRIVATE_KEY, VITE_FB_PROJECT_ID } from '$env/static/private'
 import pkg from 'firebase-admin';
 
-const privateKey = FB_PRIVATE_KEY.replace(/\\n/g, '\n');
+// const privateKey = FB_PRIVATE_KEY.replace(/\\n/g, '\n');
 
 try {
   pkg.initializeApp({
     credential: pkg.credential.cert({
-      projectId: FB_PROJECT_ID,
-      clientEmail: FB_CLIENT_EMAIL,
-      privateKey: privateKey,
+      projectId: VITE_FB_PROJECT_ID,
+      clientEmail: VITE_FB_CLIENT_EMAIL,
+      privateKey: VITE_FB_PRIVATE_KEY,
     }),
   });
 } catch (err) {
